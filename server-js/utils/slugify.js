1bd00a225ab8bca1cfa53bb5bf3d76412d1c6da2
Dan Kolz <dan@emergentideas.com>
2025-07-11 12:29:36 -0500

    dependency update

--- server-js/utils/slugify.js
+++ server-js/utils/slugify.js
@@ -2,7 +2,17 @@ function slugify(value = '') {
 	if(typeof value !== 'string') {
 		value = value.toString()
 	}
-	return value.replace(/[^a-z0-9-.]/gi, '-').toLowerCase();
+	value = value.replace(/[^a-z0-9-.]/gi, '-').toLowerCase();
+	while(value.startsWith('-')) {
+		value = value.substring(1)
+	}
+	while(value.endsWith('-')) {
+		value = value.substring(0, value.length - 1)
+	}
+	while(value.includes('--')) {
+		value = value.split('--').join('-')
+	}
+	return value
 }
 
 module.exports = slugify
